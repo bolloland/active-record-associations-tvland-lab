73bfaaa27b4853a8599868acf75523feb83c1439
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-01-30 13:57:00 -0600

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+    def actors_list
+        # binding.pry
+        #in Show > all characters > actors'rake names, grab each actors name
+        self.actors.map do |a|
+            a.full_name
+        end
+    end
 end
